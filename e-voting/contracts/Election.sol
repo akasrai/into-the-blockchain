ee7e4d89ff81fac52304a5b6c649cf37ffdfea23
akash <akasky70@gmail.com>
2020-03-29 18:01:21 +0545

    add functionality to vote

--- e-voting/contracts/Election.sol
+++ e-voting/contracts/Election.sol
@@ -5,12 +5,12 @@ contract Election {
     struct Candidate {
         uint256 id;
         string name;
-        uint256 voteCOunt;
+        uint256 voteCount;
     }
 
-    // Map (like associative array) to store candidates in key value pair
+    // Map (like associative array) to store data in key value pair
+    mapping(address => bool) public voters;
     mapping(uint256 => Candidate) public candidates;
-
     uint256 public candidateCount;
 
     constructor() public {
@@ -21,6 +21,16 @@ contract Election {
     function addCandidate(string memory _name) private {
         candidateCount++;
         candidates[candidateCount] = Candidate(candidateCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        require(!voters[msg.sender], "Already voted");
+        require(
+            _candidateId > 0 && _candidateId <= candidateCount,
+            "Not a valid candidate"
+        );
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
     }
 
 }
--- e-voting/test/election.js
+++ e-voting/test/election.js
@@ -37,4 +37,86 @@ contract('Election', function(accounts) {
         assert.equal(candidate[2], 0, 'contains correct vote count');
       });
   });
+
+  it('allows voter to vote', function() {
+    return Election.deployed()
+      .then(function(instance) {
+        candidateId = 1;
+        electionInstance = instance;
+
+        return electionInstance.vote(candidateId, { from: accounts[0] });
+      })
+      .then(function(receipt) {
+        return electionInstance.voters(accounts[0]);
+      })
+      .then(function(voted) {
+        assert(voted, 'The voter is marked voted');
+
+        return electionInstance.candidates(candidateId);
+      })
+      .then(function(candidate) {
+        var voteCount = candidate[2];
+        assert.equal(voteCount, 1, "increments the candidate's vote count");
+      });
+  });
+
+  it('throws an exception for invalid candiates', function() {
+    return Election.deployed()
+      .then(function(instance) {
+        electionInstance = instance;
+
+        return electionInstance.vote(99, { from: accounts[1] });
+      })
+      .then(assert.fail)
+      .catch(function(error) {
+        console.log(error.reason);
+        assert(error.reason);
+
+        return electionInstance.candidates(1);
+      })
+      .then(function(candidate1) {
+        var voteCount = candidate1[2];
+        assert.equal(voteCount, 1, 'candidate 1 did not receive any votes');
+
+        return electionInstance.candidates(2);
+      })
+      .then(function(candidate2) {
+        var voteCount = candidate2[2];
+        assert.equal(voteCount, 0, 'candidate 2 did not receive any votes');
+      });
+  });
+
+  it('throws an exception for double voting', function() {
+    return Election.deployed()
+      .then(function(instance) {
+        electionInstance = instance;
+        candidateId = 2;
+        electionInstance.vote(candidateId, { from: accounts[1] });
+
+        return electionInstance.candidates(candidateId);
+      })
+      .then(function(candidate) {
+        var voteCount = candidate[2];
+        assert.equal(voteCount, 1, 'accepts first vote');
+        // Try to vote again
+        return electionInstance.vote(candidateId, { from: accounts[1] });
+      })
+      .then(assert.fail)
+      .catch(function(error) {
+        assert(error.reason);
+        console.log(error.reason);
+
+        return electionInstance.candidates(1);
+      })
+      .then(function(candidate1) {
+        var voteCount = candidate1[2];
+        assert.equal(voteCount, 1, 'candidate 1 did not receive any votes');
+
+        return electionInstance.candidates(2);
+      })
+      .then(function(candidate2) {
+        var voteCount = candidate2[2];
+        assert.equal(voteCount, 1, 'candidate 2 did not receive any votes');
+      });
+  });
 });
